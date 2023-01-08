6d37e61a39cd92b264c6d932e16df148c9c6053c
alifikriUNIBA <122199610+alifikriUNIBA@users.noreply.github.com>
2023-01-08 16:02:32 +0700

    Membuat file utama.java
    ini hasil project saya kak, atas nama ali fikri dari infor A

---
+++ ali_fikri1.java
@@ -0,0 +1,15 @@
+package ali_fikri;
+
+import fikri.Condition;
+import fikri.mahasiswa1;
+import java.io.*;
+
+
+public class Mahasiswa2 {
+    public static void main (String[] args) throws IOException{
+        do{
+            Condition.percabangan();
+        }while(mahasiswa1.isRunning);
+        }
+    }
+   
